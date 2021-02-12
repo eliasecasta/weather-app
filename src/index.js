6ef5ed5bf5ea488f9a77a5bd2714da71783cc153
e71az <e71az@icloud.com>
2021-02-12 08:48:07 +0000

    Created getCity function to display the correct city information queried

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import "./styles.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery'
 
-console.log("hello world!");
+async function getCity() {
+    const cityName = $(".city-name").val()
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=934ea5f8bc3f198fabf59b607a2fcc71`
+    
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    const response = await fetch(API, requestOptions)
+    const finalResponse = await response.json()
+
+    console.log(API, response, finalResponse)
+    
+
+    // $(".content-city-name").val(finalResponse)
+}
+
+window.getCity = getCity
