68b4541a65321a70924a5d8bae9bed9f0ff4ab10
e71az <e71az@icloud.com>
2021-02-17 10:42:23 +0000

    Added a try/catch block to handle errors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 const dayGIF = 'https://mondrian.mashable.com/wp-content%252Fuploads%252F2013%252F04%252Fpalm-trees.gif%252Ffull-fit-in__1200x2000.gif?signature=u6Pkj2nxfqX8m9jTaqwq1Xl6iqM=&source=http%3A%2F%2Fmashable.com';
 const nightGIF = 'https://i.gifer.com/VTNI.gif';
 
-let getCity = async () => {
+const getCity = async () => {
   const cityInput = $('.city-name');
   const cityName = cityInput.val();
   cityInput.val('');
@@ -22,13 +22,28 @@ let getCity = async () => {
     API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=934ea5f8bc3f198fabf59b607a2fcc71`;
   }
 
-  const response = await fetch(API);
-  const city = await response.json();
+  let response = '';
+  let city = '';
+
+  try {
+    response = await fetch(API);
+
+    if (response.ok === false) {
+      throw Error('The city provided does not exist, a default city will be used');
+    }
+
+    city = await response.json();
+  } catch (error) {
+    API = 'https://api.openweathermap.org/data/2.5/weather?q=tokyo&units=metric&appid=934ea5f8bc3f198fabf59b607a2fcc71';
+    response = await fetch(API);
+    city = await response.json();
+    alert(error);
+  }
 
   return city;
-}
+};
 
-let displayDetails = (city) => {
+const displayDetails = (city) => {
   const cityName = $('.content-city-name');
   const cityTemperature = $('.content-city-temperature');
   const cityTime = $('.content-city-time');
@@ -62,9 +77,9 @@ let displayDetails = (city) => {
   return currentTime._i;
 
   /* eslint-enable no-underscore-dangle */
-}
+};
 
-let dayOrNight = (city, unixTime) => {
+const dayOrNight = (city, unixTime) => {
   const day = city.sys.sunrise;
   const night = city.sys.sunset;
   unixTime = unixTime.toString().slice(0, -3);
@@ -74,7 +89,7 @@ let dayOrNight = (city, unixTime) => {
     return 'day';
   }
   return 'night';
-}
+};
 
 $(() => {
   $('#chkSwitch').change(function toggleTemps() {
@@ -105,7 +120,7 @@ $(() => {
   });
 });
 
-let initApp = async () => {
+const initApp = async () => {
   const city = await getCity();
   const unixTime = await displayDetails(city);
   const dayNight = await dayOrNight(city, unixTime);
@@ -121,7 +136,7 @@ let initApp = async () => {
     $('.bg').css('background-image', `url(${dayGIF})`);
   }
   $('.switch.ios').css('display', 'block');
-}
+};
 
 $('#query-city').submit((e) => {
   e.preventDefault();
